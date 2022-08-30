eb9c9817b12f61d1bdbc25e5c1f30c377bb5a752
GedeonTS <gedeontshobohwa1@gmail.com>
2022-08-30 17:17:49 +0200

    Create  recipe database

---
+++ db/migrate/20220830151315_create_recipe.rb
@@ -0,0 +1,12 @@
+class CreateRecipe < ActiveRecord::Migration[7.0]
+  def change
+    create_table :recipes do |t|
+      t.string :name
+      t.integer :preparation_time
+      t.integer :cooking_time
+      t.boolean :public
+
+      t.timestamps
+    end
+  end
+end
